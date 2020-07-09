351f73a878f7239de4e405a548261b0a0ce9bde2
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-07-09 12:55:37 +0100

    base: Add basic print helpers
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- base.sh
+++ base.sh
@@ -1,3 +1,24 @@
 #!/bin/true
 
 # Common functionality between all stages
+
+# Emit a watning to tty
+function printWarning()
+{
+    echo -en '\e[1m\e[93m[WARNING]\e[0m '
+    echo $*
+}
+
+# Emit an error to tty
+function printError()
+{
+    echo -en '\e[1m\e[91m[ERROR]\e[0m '
+    echo $*
+}
+
+# Emit info to tty
+function printInfo()
+{
+    echo -en '\e[1m\e[94m[INFO]\e[0m '
+    echo $*
+}
