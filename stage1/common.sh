82ea4297260537ed04493a86c0247c4c4f01bf57
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-07-09 13:24:16 +0100

    stage1: Set CC and CXX as appropriate
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage1/common.sh
+++ stage1/common.sh
@@ -3,3 +3,14 @@
 . $(dirname $(realpath -s $0))/../base.sh
 
 # Set up stage1 specific requirements
+
+if [[ `which clang 2>/dev/null` ]]; then
+    export CC="clang"
+    export CXX="clang++"
+elif [[ `which gcc 2>/dev/null` ]]; then
+    export CC="gcc"
+    export CXX="g++"
+else
+    printError "No usable compiler was found"
+    exit 1
+fi
