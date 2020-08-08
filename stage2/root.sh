e89f9e1923a6779eb7fc23452e76d44af702d348
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-08 14:14:22 +0100

    root: Set a temporary layout (subject to change)
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage2/root.sh
+++ stage2/root.sh
@@ -4,3 +4,14 @@ set -e
 . $(dirname $(realpath -s $0))/common.sh
 
 printInfo "Configuring root filesystem layout"
+
+install -v -D -d -m 00755 "${SERPENT_INSTALL_DIR}"/usr/{bin,lib,share,sbin,include}
+install -v -D -d -m 00755 "${SERPENT_INSTALL_DIR}"/{etc,proc,run,var}
+
+install -v -D -d -m 00755 "${SERPENT_INSTALL_DIR}/run/lock"
+ln -sv ../run/lock "${SERPENT_INSTALL_DIR}/var/lock"
+
+ln -sv lib "${SERPENT_INSTALL_DIR}/usr/lib64"
+ln -sv usr/bin "${SERPENT_INSTALL_DIR}/bin"
+ln -sv usr/lib "${SERPENT_INSTALL_DIR}/lib"
+ln -sv usr/lib64 "${SERPENT_INSTALL_DIR}/lib64"
