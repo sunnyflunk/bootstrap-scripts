626ea1386928475c3856c67f4ebc6072993e9c21
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-08 13:52:23 +0100

    stage2: Add placeholder script to build the root layout
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ stage2/root.sh
@@ -0,0 +1,6 @@
+#!/bin/true
+set -e
+
+. $(dirname $(realpath -s $0))/common.sh
+
+printInfo "Configuring root filesystem layout"
---
+++ stage2/stage2.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export SERPENT_STAGE_NAME="stage2"
+
+. $(dirname $(realpath -s $0))/../base.sh
+
+executionPath=$(dirname $(realpath -s $0))
+
+COMPONENTS=(
+    "root"
+)
+
+prefetchSources
+
+for component in ${COMPONENTS[@]} ; do
+    /usr/bin/env -S -i bash --norc --noprofile "${executionPath}/${component}.sh"
+done
