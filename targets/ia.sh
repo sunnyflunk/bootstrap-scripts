735b41f6f4b9302d8f3cb8ad4afef66163f77fdd
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-11 12:32:35 +0100

    targets/ia: Prefer avx128 over 256
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- targets/ia.sh
+++ targets/ia.sh
@@ -3,6 +3,6 @@
 . $(dirname $(realpath -s $0))/../lib/base.sh
 
 
-export SERPENT_TARGET_CFLAGS="-march=haswell -mtune=skylake -O3 -fPIC"
+export SERPENT_TARGET_CFLAGS="-march=haswell -mtune=skylake -O3 -fPIC -mprefer-vector-width=128"
 export SERPENT_TARGET_CXXFLAGS="${SERPENT_TARGET_CFLAGS}"
 export SERPENT_TRIPLET="x86_64-serpent-linux-musl"
