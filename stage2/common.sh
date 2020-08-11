3dccd6e68c3ff69bcbd9274474185140d7820ba3
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-11 12:35:23 +0100

    stage2: Ensure we can link locally
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- stage2/common.sh
+++ stage2/common.sh
@@ -38,7 +38,9 @@ export AR="llvm-ar"
 export RANLIB="llvm-ranlib"
 export STRIP="llvm-strip"
 
-export CFLAGS="${SERPENT_TARGET_CFLAGS}"
-export CXXFLAGS="${SERPENT_TARGET_CXXFLAGS}"
+export CFLAGS="${SERPENT_TARGET_CFLAGS} -I${SERPENT_INSTALL_DIR}/usr/include -L${SERPENT_INSTALL_DIR}/usr/lib"
+export CFLAGS="${SERPENT_TARGET_CXXFLAGS} -I${SERPENT_INSTALL_DIR}/usr/include -L${SERPENT_INSTALL_DIR}/usr/lib"
+export LDFLAGS="${SERPENT_TARGET_LDFLAGS} -L${SERPENT_INSTALL_DIR}/usr/lib"
+export PKG_CONFIG_PATH="${SERPENT_INSTALL_DIR}/usr/lib/pkgconfig:${SERPENT_INSTALL_DIR}/usr/share/pkgconfig"
 
 prepareBuild
