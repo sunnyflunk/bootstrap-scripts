4b9ba5925fb3bff34564ec389878f2aaed1a9368
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-08-11 12:35:41 +0100

    targets/ia: Placeholder LDFLAGS for now
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- targets/ia.sh
+++ targets/ia.sh
@@ -5,4 +5,5 @@
 
 export SERPENT_TARGET_CFLAGS="-march=haswell -mtune=skylake -O3 -fPIC -mprefer-vector-width=128"
 export SERPENT_TARGET_CXXFLAGS="${SERPENT_TARGET_CFLAGS}"
+export SERPENT_TARGET_LDFLAGS=""
 export SERPENT_TRIPLET="x86_64-serpent-linux-musl"
