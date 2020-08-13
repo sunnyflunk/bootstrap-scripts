24db6c39a5cc296031a73dc4be98a9ae81291ccc
Peter O'Connor <sunnyflunk@serpentos.com>
2020-08-13 16:59:51 +1000

    stage2: Add python - will need testing in stage3
    Signed-off-by: Peter O'Connor <sunnyflunk@serpentos.com>

---
+++ stage2/python.sh
@@ -0,0 +1,35 @@
+#!/bin/true
+set -e
+
+. $(dirname $(realpath -s $0))/common.sh
+
+extractSource python
+cd Python-*
+
+# Some files aren't easily guessable during cross-compiling
+export NM="llvm-nm"
+export OBJDUMP="llvm-objdump"
+export READELF="llvm-readelf"
+echo "ac_cv_file__dev_ptmx=no
+ac_cv_file__dev_ptc=no" > config.site
+
+printInfo "Configuring python"
+CONFIG_SITE=config.site ./configure --prefix=/usr \
+    --target="${SERPENT_TRIPLET}" \
+    --host="${SERPENT_HOST}" \
+    --build="${SERPENT_ARCH}" \
+    --libdir=/usr/lib \
+    --bindir=/usr/bin \
+    --sbindir=/usr/sbin \
+    --without-cxx-main \
+    --without-ensurepip \
+    --with-system-expat \
+    --with-system-ffi \
+    --disable-ipv6
+
+
+printInfo "Building python"
+make -j "${SERPENT_BUILD_JOBS}"
+
+printInfo "Installing python"
+make -j "${SERPENT_BUILD_JOBS}" install DESTDIR="${SERPENT_INSTALL_DIR}"
--- stage2/stage2.sh
+++ stage2/stage2.sh
@@ -38,6 +38,7 @@ COMPONENTS=(
     "expat"
     "db"
     "perl"
+    "python"
 )
 
 prefetchSources
